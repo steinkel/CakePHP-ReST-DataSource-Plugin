ffa3d302727a76e15bf1e7a47e98df263f84be5f
nojimage <nojimage@gmail.com>
2012-03-08 20:42:53 +0900

    Add getLog()

--- Model/Datasource/RestSource.php
+++ Model/Datasource/RestSource.php
@@ -2,6 +2,7 @@
 
 App::uses('DataSource', 'Model/Datasource');
 App::uses('Xml', 'Utility');
+App::uses('View', 'View');
 
 /**
  * DataSource for interacting with REST APIs
@@ -26,6 +27,50 @@ class RestSource extends DataSource {
  * @var HttpSocket
  */
 	public $Http = null;
+
+/**
+ * Time the last request took
+ *
+ * @var integer
+ */
+	public $took = null;
+
+/**
+ * Request count.
+ *
+ * @var integer
+ */
+	protected $_requestCnt = 0;
+
+/**
+ * Total duration of all request.
+ *
+ * @var integer
+ */
+	protected $_requestTime = null;
+
+/**
+ * Log of request executed by this DataSource
+ *
+ * @var array
+ */
+	protected $_requestLog = array();
+
+/**
+ * Maximum number of items in request log
+ *
+ * This is to prevent query log taking over too much memory.
+ *
+ * @var integer Maximum number of request in the request log.
+ */
+	protected $_requestLogMax = 200;
+
+/**
+ * Request log limit per entry in bytes
+ *
+ * @var integer Request log limit per entry in bytes
+ */
+	protected $_requestLogLimitBytes = 256;
 
 /**
  * Loads HttpSocket class
@@ -110,9 +155,14 @@ class RestSource extends DataSource {
 		// Remove unwanted elements from request array
 		$request = array_intersect_key($request, $this->Http->request);
 
+		$timerStart = microtime(true);
 		// Issues request
 		$response = $this->Http->request($request);
 		/* @var $response HttpResponse */
+
+		// Record log
+		$this->took = round(microtime(true) - $timerStart, 3) * 1000;
+		$this->logRequest($request, $response);
 
 		// Get content type header
 		$contentType = $this->Http->response['header']['Content-Type'];
@@ -157,6 +207,91 @@ class RestSource extends DataSource {
 		}
 
 		return $response;
+	}
+
+/**
+ * Log given HTTP request.
+ *
+ * @param array $request request data
+ * @param array $response response data
+ * @return void
+ */
+	public function logRequest($request, $response) {
+		$this->_requestCnt++;
+		$this->_requestTime += $this->took;
+		$separator = '[**snip**]';
+		$maxlength = $this->_requestLogLimitBytes - strlen($separator);
+
+		$requestUri = $this->Http->url($request['uri']);
+		$requestBody = $this->Http->request['body'];
+		$responseBody = $this->Http->response['body'];
+		if (strlen($requestBody) > $this->_requestLogLimitBytes) {
+			$requestBody = substr_replace($requestBody, $separator, $maxlength / 2, strlen($requestBody) - $maxlength);
+		}
+		if (strlen($responseBody) > $this->_requestLogLimitBytes) {
+			$responseBody = substr_replace($responseBody, $separator, $maxlength / 2, strlen($responseBody) - $maxlength);
+		}
+		$this->_requestLog[] = array(
+			'request_method' => $request['method'],
+			'request_uri' => $requestUri,
+			'request_body' => h($requestBody),
+			'response_code' => $this->Http->response['status']['code'],
+			'response_type' => $this->Http->response['header']['Content-Type'],
+			'response_size' => strlen($this->Http->response['body']),
+			'response_body' => h($responseBody),
+			'query' => $request['method'] . ' ' . $requestUri,
+			'params' => '',
+			'error' => '',
+			'affected' => '',
+			'numRows' => strlen($this->Http->response['body']),
+			'took' => $this->took
+		);
+		if (count($this->_requestLog) > $this->_requestLogMax) {
+			array_pop($this->_requestLog);
+		}
+	}
+
+/**
+ * Get the request log as an array.
+ *
+ * @param boolean $sorted Get the request sorted by time taken, defaults to false.
+ * @param boolean $clear If True the existing log will cleared.
+ * @return array Array of queries run as an array
+ */
+	public function getLog($sorted = false, $clear = true) {
+		if ($sorted) {
+			$log = sortByKey($this->_requestLog, 'took', 'desc', SORT_NUMERIC);
+		} else {
+			$log = $this->_requestLog;
+		}
+		if ($clear) {
+			$this->_requestLog = array();
+		}
+		return array('log' => $log, 'count' => $this->_requestCnt, 'time' => $this->_requestTime);
+	}
+
+/**
+ * Outputs the contents of the queries log. If in a non-CLI environment the sql_log element
+ * will be rendered and output.  If in a CLI environment, a plain text log is generated.
+ *
+ * @param boolean $sorted Get the queries sorted by time taken, defaults to false.
+ * @return void
+ */
+	public function showLog($sorted = false) {
+		$log = $this->getLog($sorted, false);
+		if (empty($log['log'])) {
+			return;
+		}
+		if (PHP_SAPI != 'cli') {
+			$controller = null;
+			$View = new View($controller, false);
+			$View->set('logs', array($this->configKeyName => $log));
+			echo $View->element('request_dump', array('_forced_from_dbo_' => true), array('plugin' => 'Rest'));
+		} else {
+			foreach ($log['log'] as $k => $i) {
+				print (($k + 1) . ". {$i['request_uri']}\n");
+			}
+		}
 	}
 
 }
--- Test/Case/Model/Datasource/RestSourceTest.php
+++ Test/Case/Model/Datasource/RestSourceTest.php
@@ -11,31 +11,25 @@ class RestTestModel extends AppModel {
 	public $request = array();
 
 }
-
+ConnectionManager::create('rest_test', array(
+			'datasource' => 'Rest.RestSource',
+			'database' => false,
+		));
 /**
  * @property RestTestModel $Model
  */
 class RestSourceTestCase extends CakeTestCase {
 
-	public static function setUpBeforeClass() {
-		ConnectionManager::create('rest_test', array(
-			'datasource' => 'Rest.RestSource',
-			'database' => false,
-		));
-	}
-
-	public static function tearDownAfterClass() {
-		ConnectionManager::drop('rest_test');
-	}
-
 	public function setUp() {
 		parent::setUp();
 		$this->Model = ClassRegistry::init('RestTestModel');
+		$this->DataSource = ConnectionManager::getDataSource('rest_test');
 	}
 
 	public function tearDown() {
 		unset($this->Model);
 		parent::tearDown();
+		ob_flush();
 	}
 
 /**
@@ -69,6 +63,53 @@ class RestSourceTestCase extends CakeTestCase {
 		$results = $this->Model->find('all');
 
 		$this->assertArrayHasKey('rss', $results);
+	}
+
+/**
+ * testLog method
+ *
+ * @outputBuffering enabled
+ * @return void
+ */
+	public function testLog() {
+		$this->Model->request = array(
+			'uri' => array(
+				'host' => 'search.twitter.com',
+				'path' => 'search.json',
+				'query' => array('q' => 'twitter'),
+			),
+		);
+		$this->Model->find('all');
+		$this->Model->request = array(
+			'uri' => array(
+				'host' => 'bakery.cakephp.org',
+				'path' => 'articles.rss',
+			),
+		);
+		$this->Model->find('all');
+
+		$log = $this->Model->getDataSource()->getLog(false, false);
+		$this->assertEquals('http://search.twitter.com/search.json?q=twitter', $log['log'][0]['request_uri']);
+		$this->assertEquals('http://bakery.cakephp.org/articles.rss', $log['log'][1]['request_uri']);
+		$this->assertGreaterThan(0, $log['log'][0]['took']);
+		$this->assertGreaterThan(0, $log['log'][1]['took']);
+
+		$oldDebug = Configure::read('debug');
+		Configure::write('debug', 2);
+		ob_start();
+		$this->Model->getDataSource()->showLog();
+		$contents = ob_get_clean();
+		$this->assertRegExp('/' . preg_quote('http://search.twitter.com/search.json?q=twitter', '/') . '/s', $contents);
+		$this->assertRegExp('/' . preg_quote('http://bakery.cakephp.org/articles.rss', '/') . '/s', $contents);
+
+		ob_start();
+		$this->Model->getDataSource()->showLog(true);
+		$contents = ob_get_clean();
+
+		$this->assertRegExp('/' . preg_quote('http://search.twitter.com/search.json?q=twitter', '/') . '/s', $contents);
+		$this->assertRegExp('/' . preg_quote('http://bakery.cakephp.org/articles.rss', '/') . '/s', $contents);
+
+		Configure::write('debug', $oldDebug);
 	}
 
 }
